1b3bd1e9cedb20ad7ddfdabf7cad9fbd5b6c8309
Ulaş Saçlı <ulassacli@outlook.com>
2023-02-22 18:11:32 +0300

    upz
    upz

--- server/api/audio/listings/index.post.js
+++ server/api/audio/listings/index.post.js
@@ -1,57 +1,57 @@
-// import Joi from "joi";
+import Joi from "joi";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// const schema = Joi.object({
-//   name: Joi.string().required(),
-//   price: Joi.number().min(0).required(),
-//   category: Joi.string().required(),
-//   processing: Joi.string().required(),
-//   description: Joi.string().min(10).required(),
-//   lister_id: Joi.string().required(),
-//   audio: Joi.string().required(),
-//   user_name: Joi.string().required(),
-//   producer_id: Joi.number().required(),
-// });
+const schema = Joi.object({
+  name: Joi.string().required(),
+  price: Joi.number().min(0).required(),
+  category: Joi.string().required(),
+  processing: Joi.string().required(),
+  description: Joi.string().min(10).required(),
+  lister_id: Joi.string().required(),
+  audio: Joi.string().required(),
+  user_name: Joi.string().required(),
+  producer_id: Joi.number().required(),
+});
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  // const { error, value } = await schema.validate(body);
+  const { error, value } = await schema.validate(body);
 
-  // if (error) {
-  //   throw createError({
-  //     statusCode: 412,
-  //     statusMessage: error.message,
-  //   });
-  // }
+  if (error) {
+    throw createError({
+      statusCode: 412,
+      statusMessage: error.message,
+    });
+  }
 
   //body destructure'ı ile yapmak
-  // const {
-  //   name,
-  //   price,
-  //   category,
-  //   processing,
-  //   description,
-  //   lister_id,
-  //   audio,
-  //   user_name,
-  //   producer_id,
-  // } = body;
+  const {
+    name,
+    price,
+    category,
+    processing,
+    description,
+    lister_id,
+    audio,
+    user_name,
+    producer_id,
+  } = body;
 
   //database event
   const AudioListing = await prisma.AudioListings.create({
     data: {
-      name: body.name,
-      price: body.price,
-      category: body.category,
-      processing: body.processing,
-      description: body.description,
-      lister_id: body.lister_id,
-      audio: body.audio,
-      user_name: body.user_name,
-      producer_id: body.producer_id,
+      name,
+      price,
+      category,
+      processing,
+      description,
+      lister_id,
+      audio,
+      user_name,
+      producer_id,
     },
   });
 
