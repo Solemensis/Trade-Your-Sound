72acd7b6cc815c50ad6783ff909a474ccd179af2
Ulaş Saçlı <ulassacli@outlook.com>
2023-02-22 15:20:25 +0300

    upzy

--- server/api/audio/listings/index.post.js
+++ server/api/audio/listings/index.post.js
@@ -1,31 +1,31 @@
-import Joi from "joi";
+// import Joi from "joi";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const schema = Joi.object({
-  name: Joi.string().required(),
-  price: Joi.number().min(0).required(),
-  category: Joi.string().required(),
-  processing: Joi.string().required(),
-  description: Joi.string().min(10).required(),
-  lister_id: Joi.string().required(),
-  audio: Joi.string().required(),
-  user_name: Joi.string().required(),
-  producer_id: Joi.number().required(),
-});
+// const schema = Joi.object({
+//   name: Joi.string().required(),
+//   price: Joi.number().min(0).required(),
+//   category: Joi.string().required(),
+//   processing: Joi.string().required(),
+//   description: Joi.string().min(10).required(),
+//   lister_id: Joi.string().required(),
+//   audio: Joi.string().required(),
+//   user_name: Joi.string().required(),
+//   producer_id: Joi.number().required(),
+// });
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  const { error, value } = await schema.validate(body);
+  // const { error, value } = await schema.validate(body);
 
-  if (error) {
-    throw createError({
-      statusCode: 412,
-      statusMessage: error.message,
-    });
-  }
+  // if (error) {
+  //   throw createError({
+  //     statusCode: 412,
+  //     statusMessage: error.message,
+  //   });
+  // }
 
   //body destructure'ı ile yapmak
   const {
