c6150ea729e42d0c785abb3a2ad71f81a8a8f593
Ulaş Saçlı <ulassacli@outlook.com>
2023-02-22 19:18:20 +0300

    simple thing
    simple thing

--- middleware/specificuser.js
+++ middleware/specificuser.js
@@ -1,6 +1,4 @@
 export default async function defineNuxtRouteMiddleware(to, from) {
-  const user = useSupabaseUser();
-
   const specificUser = await $fetch("/api/producerProfile/specificUser");
 
   if (specificUser.user_name) {
