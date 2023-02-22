ab20c5a14ec7d1e3a797cfbbff505f4a52f6b658
Ulaş Saçlı <ulassacli@outlook.com>
2023-02-22 04:59:25 +0300

    upzy
    upzy

--- pages/chat.vue
+++ pages/chat.vue
@@ -9,30 +9,27 @@ const supabase = useSupabaseClient();
 //fetch chats according to logged user
 const chatRooms = ref([]);
 onMounted(async () => {
-  // setTimeout(async () => {
-  //   const { data, error, refresh, pending } = await useFetch(
-  //     "/api/chatroom/fetchChats",
-  //     {
-  //       method: "post",
-  //       body: user.value.id,
-  //     }
-  //   );
+  setTimeout(async () => {
+    //   const { data, error, refresh, pending } = await useFetch(
+    //     "/api/chatroom/fetchChats",
+    //     {
+    //       method: "post",
+    //       body: user.value.id,
+    //     }
+    //   );
+    const { data } = await useFetch("/api/chatroom/fetchChats");
 
-  //   if (!pending.value) {
-  //     loading.value = false;
-  //   }
+    //   if (!pending.value) {
+    //     loading.value = false;
+    //   }
 
-  //   if (!data.value && error.value) {
-  //     error.value = null;
-  //     refresh();
-  //   }
-  //   chatRooms.value = data.value;
-  // }, 1);
-
-  chatRooms.value = await $fetch("/api/chatroom/fetchChats", {
-    method: "post",
-    body: user.value.id,
-  });
+    //   if (!data.value && error.value) {
+    //     error.value = null;
+    //     refresh();
+    //   }
+    //   chatRooms.value = data.value;
+    chatRooms.value = data.value;
+  }, 1);
 });
 
 //fetch messages according to selected chat
--- server/api/chatroom/fetchChats.js
+++ server/api/chatroom/fetchChats.js
@@ -1,14 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { serverSupabaseUser } from "#supabase/server";
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-  const userId = await readBody(event);
+  const userId = await serverSupabaseUser(event);
+
   //database event
   const userRelatedChatRooms = await prisma.ChatRooms.findMany({
     where: {
       OR: [
-        { user1_deleted: { equals: { userId: userId, deleted: false } } },
-        { user2_deleted: { equals: { userId: userId, deleted: false } } },
+        { user1_deleted: { equals: { userId: userId.id, deleted: false } } },
+        { user2_deleted: { equals: { userId: userId.id, deleted: false } } },
       ],
     },
   });
